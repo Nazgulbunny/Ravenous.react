98cbb9c722bc12c76756dfcfb913cb082d8f0a9d
Max <maxsermi@gmail.com>
2017-12-29 00:06:40 +0100

    completed business js and css

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React from 'react';
+import './Business.css';
 
 class Business extends React.Component {
 render(){
 
-	
-}
-
-}
+return (
+	<div className="Business">
+	  <div className="image-container">
+	    <img src={business.this.imageSrc} alt='' />
+	  </div>
+	  	<h2>{business.this.name}</h2>
+	  <div className="Business-information">
+	    <div className="Business-address">
+	      <p>1{business.this.address}</p>
+	      <p>{business.this.city}</p>
+	      <p>{business.this.state},{business.this.zipCode}</p>
+	    </div>
+	    <div className="Business-reviews">
+	      <h3>{business.this.category}</h3>
+	      <h3 className="rating">{business.this.rating}stars</h3>
+	      <p>{business.this.reviewCount}reviews</p>
+	    </div>
+	  </div>
+	</div>
+		);
+	}
+};	
 
 
 const business = {
@@ -20,3 +39,6 @@ const business = {
 	  rating: 4.5,
 	  reviewCount: 90
 }; 
+
+
+export default Business;
