4c47c306aa9e9e6cfd50323f69cc2389048e77d1
Max <maxsermi@gmail.com>
2017-12-29 13:33:57 +0100

    fixed a bug in app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import BusinessList from '../components/BusinessList';
-import SearchBar from '../components/SearchBar';
 
-class App extends Component {
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+class App extends React.Component {
   render() {
-    return <div className="App">
-  <h1>ravenous</h1>
-  <SearchBar>
-  <BusinessList> 
-</div>
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
   }
 }
 
