6e5b98c55286f7b013343b6f323cca5432396d82
Max <maxsermi@gmail.com>
2017-12-30 16:56:44 +0100

    bug fixes in index.js and businesslist js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,15 +7,16 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        {
-          this.props.businesses.map(business => {
-            return <Business business={business} />
-          }) 
-        }
-
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
       </div>
     );
   }
 }
 
 export default BusinessList;
+
